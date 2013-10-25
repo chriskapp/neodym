c4f81ecf0d9acc5682a5cda15b8cbd3f2e88f10e
Christoph Kappestein <k42b3.x@gmail.com>
2013-10-25 22:51:40 +0200

    remive libraries

--- src/com/k42b3/neodym/Neodym.java
+++ src/com/k42b3/neodym/Neodym.java
@@ -29,7 +29,7 @@ package com.k42b3.neodym;
  */
 public class Neodym 
 {
-	public static final String VERSION = "0.0.5 beta";
+	public static final String VERSION = "0.0.6";
 	
 	public static String getVersion()
 	{
